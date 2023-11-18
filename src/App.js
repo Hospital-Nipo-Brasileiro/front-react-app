2c3da533417ef952ea15dcff8cfd47cff72de170
GustavoDeSouzaFonseca <rm94067@fiap.com.br>
2023-11-18 10:42:19 -0300

    [:construction:] Puxando info da NaoEncontrado da main

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'; // Importe também o Navigate
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import NaoEncontrado from './pages/NaoEncontrado/NaoEncontrado.jsx';
+import TechNipo from './pages/techNipo/TechNipo.jsx';
+import Login from './pages/login/Login.jsx';
+import Home from './pages/home/Home.jsx';
+import MeuUsuario from './pages/MeuUsuario/index.jsx'
+import AutomacaoUsuarios from './pages/automacaoUsuarios/AutomacaoUsuarios.jsx'
+import CentralEstoques from './pages/estoque/CentralEstoque.jsx'
+import Acessos from './pages/acessos/Acessos.jsx'
+import EstoqueTemp from './pages/estoque/EstoqueTemp.jsx'
+import EntradaEstoque from './pages/estoque/EntradaEstoque.jsx'
+import { useAuth } from './contexts/AuthContext.js'
 
-import PublicRoutes from './routes/public.routes.js';
-import PrivateRoutes from './routes/private.routes.js';
-
-function App() {
+export default function App() {
+  const { auth } = useAuth();
 
   return (
     <BrowserRouter>
-      <PublicRoutes />
-      <PrivateRoutes />
+      <Routes>
+        <Route path='/' element={<TechNipo />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={auth ? <Home /> : <Navigate to="/login" />} />
+        <Route path='/meu-usuario' element={auth ? <MeuUsuario /> : <Navigate to="/login" />} />
+        <Route path='/automation' element={auth ? <AutomacaoUsuarios /> : <Navigate to="/login" />} />
+        <Route path='/estoques/central' element={auth ? <CentralEstoques /> : <Navigate to="/login" />} />
+        <Route path='/acessos' element={auth ? <Acessos /> : <Navigate to="/login" />} />
+        <Route path='/estoques/:id' element={auth ? <EstoqueTemp /> : <Navigate to="/login" />} />
+        <Route path='/estoques/entrada' element={auth ? <EntradaEstoque /> : <Navigate to="/login" />} />
+        <Route path="*" element={<NaoEncontrado />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
-
-export default App;
---
+++ src/pages/NaoEncontrado/NaoEncontrado.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Error404 from '../../assets/404.svg'
+import { Link } from 'react-router-dom';
+
+function NaoEncontrado() {
+  return (
+    <div className="login-background">
+      <div className="app-background flex flex-col items-center justify-center">
+        <img className='w-[500px]' src={Error404} alt='portal error 404' />
+        <span className='font-sans text-[#FF5C00] text-6xl'>Página não encontrada</span>
+        <div>
+            <span className='font-sans text-white text-3xl'>retorne a página inicial, obrigado! </span>
+            <Link to="/home">
+                <span className='font-sans text-[#FF5C00] text-3xl'>clique para voltar</span>
+            </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NaoEncontrado;
