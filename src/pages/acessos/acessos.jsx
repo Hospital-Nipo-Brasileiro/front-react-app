2385bb41e673f7e4f3c148344a35b66d275564fb
Sup Gustavo de Souza Fonseca <sup.gustavo@nipo.local>
2023-11-29 17:57:07 -0300

    [:recycle:] Ajustado requisicao de acesso para puxar info de sistema por usuario

--- src/pages/acessos/acessos.jsx
+++ src/pages/acessos/acessos.jsx
@@ -2,17 +2,27 @@ import React, { useEffect, useState } from 'react';
 import NavBarUser from '../../components/NavBarUser';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import ModalCriaPessoas from '../../components/ModalCriaPessoa';
+import ModalPessoa from '../../components/ModalPessoa';
 
 
 function Acessos() {
   const [pessoas, setPessoas] = useState([]);
+  const [modalCriaPessoas, setModalCriaPessoas] = useState(false);
+  const [selectedPersonID, setSelectedPersonID] = useState(null);
   const [totalPessoas, setTotalPessoas] = useState(0);
+  const [arraySistemaPessoa, setArraySistemaPessoa] = useState([]);
+  const [name, setName] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [dataAdmissao, setDataAdmissao] = useState("");
+  const [dataNascimento, setDataNascimento] = useState("");
+  const [tipoContrato, setTipoContrato] = useState("");
+  const [categoria, setCategoria] = useState("");
 
-  const BASE_URL = "http://10.10.204.54:8080"
+  const BASE_URL = "http://HSRVWVH00028:8080"
   const token = sessionStorage.getItem('token');
-
   useEffect(() => {
-    axios.get(`${BASE_URL}/pessoas/sistemas`,
+    axios.get(`${BASE_URL}/sistemas/pessoas/filtra`,
       {
         headers: {
           'Content-Type': 'multipart/form-data',
@@ -37,6 +47,46 @@ function Acessos() {
         });
       })
   }, [token])
+  const handleOpenModalCriaPessoa = () => {
+    setModalCriaPessoas(true);
+  }
+  const handleCloseModalCriaPessoa = () => {
+    setModalCriaPessoas(false);
+  }
+
+  const handleOpenPessoa2 = (pessoaID) => {
+    axios.get(`${BASE_URL}/sistemas/pessoas/${pessoaID}/filtra`, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'Authorization': `${token}`,
+      }
+    })
+      .then((response) => {
+        setArraySistemaPessoa(response);
+        setSelectedPersonID(pessoaID);
+      })
+      .catch((err) => {
+        toast.error(`Erro ao enviar arquivos: ${err.data}`, {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      })
+
+  }
+
+  const handleOpenPessoa = (pessoaID) => {
+    setSelectedPersonID(pessoaID);
+  }
+
+  const handleClosePessoa = () => {
+    setSelectedPersonID(null);
+  }
 
   return (
     <div className="login-background">
@@ -47,51 +97,56 @@ function Acessos() {
         <section className="w-5/6 h-5/6 rounded-3xl bg-black/50 m-12 flex flex-col items-center">
           <nav className="w-full h-10 bg-white rounded-3xl shadow-md flex flex-row justify-center">
             <div className="w-10/12 flex justify-start pl-6 pr-6">
-
               <div className="flex justify-center items-center h-full mr-3 ">
                 <span className='w-[40px] font-sans font-bold'>id</span>
               </div>
-
               <div className="flex justify-start items-center h-full mr-3 w-1/3">
                 <span className='font-sans font-bold'>nome</span>
               </div>
-
               <div className="flex justify-start items-center h-full ml-3 mr-3 w-2/3">
                 <span className='font-sans font-bold'>acessos</span>
               </div>
-
             </div>
           </nav>
-
           <div className='w-5/6 h-5/6'>
-            {pessoas && pessoas.map((pessoa) => (
-              <div className='w-full h-10 bg-white rounded-3xl pl-6 pr-6 mt-8 '>
-                <div key={pessoa.ID} className="flex justify-start items-center h-full mr-3 w-full">
-                  <div className="flex justify-center items-center h-full mr-3 ">
-                    <span className='w-[40px] font-sans'>{pessoa.ID}</span>
-                  </div>
-                  <div className="flex justify-start items-center h-full mr-3 w-1/3">
-                    <span className='font-sans'>{pessoa.NOME}</span>
-                  </div>
-                  <div className="flex justify-start items-center h-full ml-3 mr-3 w-2/3">
-                    <span className='font-sans'>{pessoa?.SISTEMAS || "Nenhum sistema cadastrado"}</span>
-                  </div>
-                </div>
 
+          {modalCriaPessoas === true ? <ModalCriaPessoas onCloseModal={handleCloseModalCriaPessoa} /> : null}
+
+          {pessoas && pessoas.map((pessoa) => (
+            <div
+              className='w-full h-10 bg-white rounded-3xl pl-6 pr-6 mt-8'
+              onClick={() => handleOpenPessoa(pessoa.ID)}
+              key={pessoa.ID}
+            >
+              <div className="flex justify-start items-center h-full mr-3 w-full">
+                <div className="flex justify-center items-center h-full mr-3 ">
+                  <span className='w-[40px] font-sans'>{pessoa.ID}</span>
+                </div>
+                <div className="flex justify-start items-center h-full mr-3 w-1/3">
+                  <span className='font-sans'>{pessoa.NOME}</span>
+                </div>
+                <div className="flex justify-start items-center h-full ml-3 mr-3 w-2/3">
+                  <span className='font-sans'>{pessoa?.SISTEMAS || "Nenhum sistema cadastrado"}</span>
+                </div>
               </div>
-            ))}
+            </div>
+          ))}
+
+          {selectedPersonID !== null ? <ModalPessoa onCloseModal={handleClosePessoa} /> : null}
+
           </div>
 
           <div className='w-5/6 h-[40px] flex justify-end'>
-            <button className='w-[40px] h-[40px] bg-orange-500 rounded-full flex items-center justify-center'>
+            <button
+              className='w-[40px] h-[40px] bg-orange-500 rounded-full flex items-center justify-center'
+              onClick={handleOpenModalCriaPessoa}
+            >
               <span className='text-4xl text-white'>+</span>
             </button>
           </div>
-
         </section>
       </div>
     </div>
   )
 }
-
 export default Acessos;
